a29f690213f39ca0c5813d58d01d7267aed825ee
Manuel Mariano Silva <manuelmarianosilva@gmail.com>
2018-09-26 21:56:01 -0300

    Sign in screen layout básico implementado

---
+++ app/src/main/java/com/app/yonoc/fence/View/Login/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.app.yonoc.fence.View.Login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.yonoc.fence.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+
+
+    }
+}
